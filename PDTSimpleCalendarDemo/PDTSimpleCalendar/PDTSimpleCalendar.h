20c63193beeb574d02f7389b99e5a75501257100
Gena <g@goodworkapps.com>
2015-06-21 00:22:43 +0300

    Carthage support + messing with demos

---
+++ PDTSimpleCalendarDemo/PDTSimpleCalendar/PDTSimpleCalendar.h
@@ -0,0 +1,22 @@
+//
+//  PDTSimpleCalendar.h
+//  PDTSimpleCalendar
+//
+//  Created by Gennady Evstratov on 21.06.15.
+//  Copyright (c) 2015 Producteev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PDTSimpleCalendar.
+FOUNDATION_EXPORT double PDTSimpleCalendarVersionNumber;
+
+//! Project version string for PDTSimpleCalendar.
+FOUNDATION_EXPORT const unsigned char PDTSimpleCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PDTSimpleCalendar/PublicHeader.h>
+
+#import <PDTSimpleCalendar/PDTSimpleCalendarViewController.h>
+#import <PDTSimpleCalendar/PDTSimpleCalendarViewHeader.h>
+#import <PDTSimpleCalendar/PDTSimpleCalendarViewCell.h>
+#import <PDTSimpleCalendar/PDTSimpleCalendarViewFlowLayout.h>
